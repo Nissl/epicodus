970195b1fb9d4a066ebf1e230aec62609dd993e3
John Morgan <jtmorgan61@gmail.com>
2013-12-20 15:33:42 -0800

    simple effects

---
+++ basic_html_css/js/pop-scripts.js
@@ -0,0 +1,18 @@
+// $(function() {
+//   $("h1").click(function() {
+//     alert("This is a header.");
+//     alert("I TOLD YOU THIS IS A HEADER");
+//   });
+
+//   $("p").click(function() {
+//     alert("This is a paragraph.");
+//     $("img").show();
+//   });
+
+//   $("img").click(function() {
+//     alert("This is an image.")
+//   });
+
+
+// });
+
